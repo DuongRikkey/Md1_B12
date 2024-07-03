5593f385798d66788f7d13b1167591b4fb66db03
Duong Rikkey <sosauchin2@gmail.com>
2024-07-03 22:45:21 +0700

    Hi

---
+++ Session10JS/JS.js
@@ -0,0 +1,449 @@
+// alert("Hello");
+// prompt("Nhap vao")
+// var book ="Tôi là Ronaldo";
+// let film ="  How i meet your dog abe";
+
+// const album ="Tắt nó đi@@";
+// // alert(book)
+// //IN NHỮNG GIÁ TRỊ NẰM TRONG BIẾN
+
+// book = "Doraemon";
+// alert(book);
+// film = "hihi";
+
+// alert(film);
+// album = "1989s"
+// let fname= prompt ("Nhập đi")
+
+// alert(fname);
+
+// Viết 1 ứng dụng cho người dùng nhập vào số a + b hiện thị tổng độ ra màn hình qua alert
+// B1 Cho user nhập vào a
+// let a = Number( prompt("Nhap a"))
+// // B2 Cho user nhập vào b
+// let b = + prompt ("Nhap b")
+// // B3 tinh tong
+// let total = Number(a)+Number(b)
+// alert (total)
+// let Number1=20;
+
+// let Number2=10;
+// console.log(Number1+Number2);
+// console.log(Number1%Number2)
+// let sentence=`Hello." \n Duong`
+// let random="ABC"
+// let poem=`Nam
+// bắc`
+
+// console.log(sentence);
+// console.log(random);
+// console.log(poem);
+// let stringA="Xin chao";
+// let stringB="Im Duong";
+// console.log(stringA + stringB );
+// let stringD = "Thầy Quang";
+// let stringC =
+// `Xin chao cac ${stringD}, bạn,  tôi là Duong`
+// console.log (stringC);
+// B1
+// let math= Number(prompt("Nhập Toán"));
+// let physics =Number(prompt("Nhập LÝ"));
+// let chemistry =Number(prompt("Nhập Hóa"));
+// let average=(math + physics + chemistry)/3;
+// alert("average");
+
+// B2
+// let USD= Number(prompt("mới bạn nhập tiền vào Sv"));
+// const $1 =  23000  ;
+// let vnd=USD*$1;
+// alert(vnd);20
+
+// B3
+// let a = Number(prompt("Mời bạn nhập vào"));
+// let b = Number(prompt("Mời bạn nhập vào"));
+// let c = Number(prompt("Mời bạn nhập vào"));
+
+// let max=a;
+// if(b>max) {
+//     b=max;
+// }
+// if(c>max){
+//     c=max;
+// }
+// console.log("Hiện kqua" +max);
+
+// let a=5;
+// let b="5";
+// console.log (a===b);
+
+// let DTB= Number(prompt("Mời nhập vào"));
+// if(DTB>8 ){
+//     console.log(`HSG`);
+// } else if(5<DTB<8 ) {
+//     console.log(`KhA`);
+// } else {
+//     console.log(`Kém`)
+// }
+
+// let a=Number(prompt("Mời bạn nhập cạnh a"));
+// let b=Number(prompt("Mời bạn nhập cạnh b"));
+// ;
+
+// let result = a > b ? "a lon hon b" : "a be hon b"
+// alert(result);
+
+// Vẽ hình kim tự tháp ngược có 5 dòng
+
+// for (let i = 5; i >= 1; i--) {
+//     let col ='';
+//     for (let j=1;j<=i; j++){
+//     col +='#'
+// }
+//    console.log(col)
+//
+
+// for(i=1; i<5 ;i++){
+//     let row="";
+//     for(j=1,j<=i,j++){
+//         col +=''
+//     }
+
+// }
+// let Score = Number(prompt("Nhập điểm")) ;
+// if(Score > 75) {
+//     console.log (`A`);
+// }
+//  else if (75> Score >60) { console.log (`B`);  }
+// else if (60> Score >45) { console.log (`C`);  }
+
+// let DTB= Number(prompt("Mời nhập vào"));
+// if(DTB>8 ){
+//     console.log(`HSG`);
+// } else if (5< DTB && DTB <8 ) {
+//     console.log(`KhA`);
+// } else {
+//     console.log(`Kém`)
+// }
+// let Score = Number (prompt("Mời m nhập nha"));
+// if ( Score  >= 75 &&  Score < 100) {
+//     console.log("A");
+// }
+// else if (Score >= 60 && Score < 75) {
+//     console.log("B");
+// }
+// else if (Score >= 45 && Score < 60 ) {
+//     console.log("C");
+// }
+// else if (Score > 35 && Score <45 ) {
+//     console.log("D");
+
+//  } else if (Score >= 0 && Score <= 35) {
+//         console.log("E");}
+// else {
+//     console.log("Chịu")
+// }
+
+// let sentence ="Hihihi"
+
+// console.log(sentence[0]);
+// console.log(sentence[2]);
+// let sentence2="ex laudantium eius voluptatem "
+
+// console.log(sentence2.length)
+
+// let robeo ="VVV bbb NJH"
+// robeo.toUpperCase();
+// console.log(robeo)
+// let anryUpper=robeo.toUpperCase()
+// console.log(anryUpper)
+// let Duong=" mmmmmmmmmmmmmmm        "
+// console.log(Duong.trim() )
+// let DuongStar =Duong.trim()
+// console.log(DuongStar)
+
+// let random= null;
+// console.log(random)
+// let a = 10
+// let x = a++ + a ;
+// console.log(x)
+
+// if (Score > 75 && Score <= 100) {
+//     console.log("A");
+// } else if (Score > 60 && Score <= 75) {
+//     console.log("B");
+// } else if (Score > 45 && Score <= 60) {
+//     console.log("C");
+// } else if (Score > 35 && Score <= 45) {
+//     console.log("D");
+// } else if (Score >= 0 && Score <= 35) {
+//     console.log("E");
+// } else {
+//     console.log("Điểm không hợp lệ");
+// }
+
+// const numbers = [5, 7, 9, 12];
+// let sum=0
+// for (const number of numbers) {
+//     sum += number;
+// }
+// console.log("Tổng các phần tử trong mảng là:", sum);
+
+// X =[ 4,5,7, 9, 12];
+// let sum =X[0];
+// // for(let value of X){
+// //     console.log(value);
+// //     sum=sum+value;
+// // }
+
+// // console.log(sum);
+
+// let sum = 0;
+// for (let i = 1; i < 5; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// const X = [5, 8, "15", "10", 21, 25, 30, "5"];
+// let sum = 0;
+// for (let value of X) {
+//   if (typeof value === "string" && value % 5 == 0) {
+//     sum = sum + value;
+//   }
+// }
+// console.log(sum);
+
+// const A = [10, "hello", 15, 20, "world", 25, 30, "JavaScript", 7, 45];
+
+// let sum = 0;
+// for (let i = 0; i < A.length; i++) {
+//   if (typeof A[i] === "number" && A[i] % 5 === 0) {
+//     sum = sum + A[i];
+//   }
+// }
+// console.log(sum);
+
+// const A = [2, 7, 9, 1, 6, 44, 7, 23];
+
+// let sumchan = 0;
+// let sumle = 0;
+// for (i = 0; i < A.length; i = i + 1) {
+//   if (A[i] % 2 === 0) {
+//     sumchan = sumchan + A[i];
+//   } else {
+//     sumle = sumle + A[i];
+//   }
+// }
+// console.log(sumchan);
+// console.log(sumle);
+
+// const A = [2, 7, 9, 1, 6, 44, 7, 23];
+// let max = A[4];
+// let min = A[5];
+// for (i = 0; i < A.length; i++) {
+//   if (A[i] > max) {
+//     max = A[i];
+//   }
+//   if (A[i] < min) {
+//     min = A[i];
+//   }
+// }
+// console.log("Số lớn nhất trong mảng là", max);
+// console.log("Số bé là", min);
+
+// let A = [2, 7, 9, 1, 6, 44, 7, 23];
+
+// // for (let i = 0; i < A.length; i++)
+// //   for (let j = i + 1; j < A.length; j++)
+// //     if ((A[i]>A[j]))
+//       {
+// //     }
+// {
+// }
+
+// let chuoi =abcd,123;
+// console.log(typeof chuoi);
+
+// let arrA = [1, 2, 3, 4, 5];
+// let arrB = arrA;
+
+// console.log(arrA, arrB);
+
+//(): nơi đưa tham
+
+// let tenDuong = "Duong";
+// let tuoiDuong = 23;
+// let caoDuong = 170;
+// let hocDuong = ["tieu hoc", "trung hoc", "dai hoc"];
+
+// function inThongtinDuong(ten, tuoi, hoc) {
+//   ten = "Duog";
+//   tuoi ="30";
+//   hoc.push("giaosu");
+//   console.log(ten, tuoi, hoc);
+//   tuoi ="30";
+
+// }
+
+// inThongtinDuong(tenDuong, tuoiDuong, hocDuong);
+// function timsolonthunhat(mang, n =mang.length)
+// let mang1 = [34, 5462, 345, 3, 44];
+// for (let i = 0; i < mang1.length - 1; i++) {
+//   for (let j = i + 0; j < mang1.length; j++) {
+//     if (mang1[i] > mang1[j]) {
+//       let temp = mang1[i];
+//       mang1[i] = mang1[j];
+//       mang1[j] = temp;
+//     }
+//   }
+// }
+// console.log(mang1);
+
+// let bmi = +prompt("Mời bạn nhập vào");
+
+// if ((bmi < 18.5)) {
+//   console.log("Cân nặng thấp");
+// } else if (bmi > 18.5 && bmi < 24.9) {
+//   console.log("Bình thương");
+// } else if (bmi > 25 && bmi < 29.9) {
+//   console.log("Tiền béo phì");
+// } else {
+//   console.log("ko có chi");
+// }
+
+// let a = +prompt("Nhập vào");
+// console.log(a);
+// switch (a) {
+//   case 1: // Tháng 1
+//   case 3: // Tháng 3
+//   case 5: // Tháng 5
+//   case 7: // Tháng 7
+//   case 8: // Tháng 8
+//   case 10: // Tháng 10
+//   case 12: // Tháng 12
+//     console.log("31 days");
+//     break;
+//   case 4:
+//   case 6:
+//   case 9:
+//   case 11:
+//     console.log("30 days");
+//     break;
+//   case 2:
+//     console.log("29 days");
+//     break;
+// }
+
+// let a = +prompt("Mời bạn nhập vào");
+// let b = prompt("Nhập dấu");
+// let c = +prompt("Mời bạn nhập vào");
+// let result;
+// switch (b) {
+//   case "+":
+//     result = a + c;
+//     console.log(result);
+
+//     break;
+//   case "-":
+//     result = a - c;
+//     console.log(result);
+
+//     break;
+//   case "*":
+//     result = a * c;
+//     console.log(result);
+
+//     break;
+//   case "/":
+//     result = a / c;
+//     console.log(result);
+
+//     break;
+// }
+//
+
+// Câu10
+// let a = +prompt("Nhập số a");
+// let b = +prompt("Nhập số b");
+// let c = +prompt("Nhập số c");
+// let max;
+// if (a > b && a > c) {
+//   max = a;
+//   console.log(a);
+// } else if (b > a && b > c) {
+//   max = b;
+//   console.log(b);
+// } else {
+//   max = c;
+//   console.log(`Số lớn nhất ${max}`);}
+
+// Câu:11.9
+
+// let DTB = +prompt("Nhập số điểm");
+
+// if (DTB >= 8 && DTB <= 10) {
+//   console.log("Học giỏi");
+// } else if (DTB >= 6.5 && DTB <= 7.9) {
+//   console.log("Khá");
+// } else if (DTB >= 5 && DTB <= 6.4) {
+//   console.log("TB");
+// } else if (DTB <5 ) {
+//   console.log("Yếu");
+// }
+
+// câu 11.1
+// let tuoi = +prompt("Nhập tuổi của bạn");
+
+// if (tuoi >= 18) {
+//   console.log(`Bạn đạt yêu cầu với tuổi: ${tuoi}`);
+// }
+// if (tuoi < 18) {
+//   console.log(`Bạn đạt yêu cầu với tuổi: ${tuoi}`);
+// }
+// câu 11.10
+// let month = +prompt("Mời bạn");
+// switch (month) {
+//   case 1:
+//   case 3:
+//   case 5:
+//   case 7:
+//   case 8:
+//   case 12:
+//     console.log(`Tháng :${month} có 31 ngày`);
+//     break;
+//   case 4:
+//   case 6:
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log(`Tháng ${month} có 30 ngày`);
+//     break;
+// }
+
+// Câu11.4
+// let number = +prompt("Mời bạn nhật vào nha");
+// if (number % 2 === 0) {
+//   console.log(`Số là ${number} là chẵn`);
+// } else {
+//   console.log(`Số là ${number} là lẻ`);
+// }
+
+// Câu 11.3
+// let color = prompt("Nhập vào");
+// let choose = "";
+// if (color === "xanh" || color === "vang" || color === "do") {
+//   choose = `Hiển thị nha`;
+// } else {
+//   choose = `Tịu`;
+// }
+// console.log(choose);
+// 11.5;
+
+// let a = +prompt("mời bạn nhập số nhé");
+// switch (a % 2) {
+//   case 0:
+//     console.log("số chẵn");
+//     break;
+//   case 1:
+//     console.log("So lẻ");
+//     break;
+// }
